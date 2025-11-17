2becec7b758dbb9c9beb7d4bc34982f71fd0ec9f
Szymon Rutkowski <Szymon.rutkowski1@gmail.com>
2025-11-17 15:21:06 +0100

    reorganizing js by adding funkctions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-console.log("Krótka strona o mnie, w trakcie rozwijania. Więcej już w krótce!")
+{
+    console.log("Krótka strona o mnie, w trakcie rozwijania. Więcej już wkrótce!");
 
-let button = document.querySelector(".nav__themeButton");
-let body = document.querySelector(".page");
-let themeName = document.querySelector(".nav__themeName");
+    const toggleTheme = () => {
+        const body = document.querySelector(".page");
+        const themeName = document.querySelector(".nav__themeName");
 
-button.addEventListener("click", () => {
-    body.classList.toggle("dark");
+        body.classList.toggle("dark");
+        themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+    };
 
-    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+    const init = () => {
+        const button = document.querySelector(".nav__themeButton");
+        button.addEventListener("click", toggleTheme);
+    };
 
-});
+    init();
+}
